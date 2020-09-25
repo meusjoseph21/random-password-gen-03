c0ab48ca96a465984e4c4a7d96e8a3483423ea3d
JosMuse <no-reply>
2020-09-25 10:03:48 -0600

    added more comments

--- script.js
+++ script.js
@@ -1,15 +1,6 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
-// function writePassword() {
-//   var password = generatePassword();
-//   var passwordText = document.querySelector("#password");
-
-//   passwordText.value = password;
-
-// }
-
 
 
 generateBtn.addEventListener("click", writePassword);
@@ -38,10 +29,10 @@ var askSpecial;
 function PassOpt() {
 
 
-    var askCharLength = (prompt("please choose a character length between 8 and 128 characters."))
+    var askCharLength = (prompt("please choose a character length between 8 and 128 characters."));
   
-    //I want to make a function that does not take a response outside of 8-128 characters.
-  
+    //I want to make a function that does not take a response outside of 8-128 characters. The do/while will continuously loop if 8 and 128 criteria not met.
+
     if (askCharLength < 8 || askCharLength > 128) {
   
       do { alert("Invalid answer. Please choose between 8 and 128 characters.");
@@ -52,6 +43,7 @@ function PassOpt() {
     }
 
 
+    //Prompts to confirm what special characters the user would like to use in their password.
 
   var askNumber = confirm("would you like your password to use numbers?");
 
@@ -60,6 +52,8 @@ function PassOpt() {
   var askLower = confirm("would you like your password to use lower case letters?");
 
   var askSpecial = confirm("would you like your password to use special characters?");
+
+  //This do/while will keep the user in the statements until they choose at least one of the confirm statements to be true.
 
   if (askNumber === false && askUpper === false && askLower === false && askSpecial === false) {
     do { alert("You must choose at least one of these options.");
@@ -74,7 +68,7 @@ function PassOpt() {
 
     while (askNumber === false && askUpper === false && askLower === false && askSpecial === false)
   }
-  // this object is confirming true/false, storing answers check console
+  // this object is storing answers check console
 
   var pwOptions = {
     askCharLength : askCharLength,
@@ -96,7 +90,7 @@ function PassOpt() {
     return result;
   }
 
-  //Generate password function will confirm true/false with if statements, 
+  //Generate password function will 
 
   function generatePassword() {
     var options = PassOpt();
