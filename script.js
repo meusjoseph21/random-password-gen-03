d6458806073cd214cc9912937d93339f9871a039
JosMuse <no-reply>
2020-09-24 20:17:39 -0600

    generator works, need to add comments, clean up js file

--- script.js
+++ script.js
@@ -12,50 +12,61 @@ var generateBtn = document.querySelector("#generate");
 
 
 
-
-//--------------------Psuedocode for what I need to do-----------------------------------------
-
-// I Need a password
-
-//On click which is this->
-
 generateBtn.addEventListener("click", writePassword);
 
-//I will be prompted with confirm statements for: 
 
-//1. Character count between 8 and 128 - cant be under 8 or over 128. 
+//ALL GENERATE RANDOM FROM MY ARRAYS
+// var randNumb = Math.floor(Math.random() * passCriteria.charNumber.length);
+// var randUpper = Math.floor(Math.random() * passCriteria.charUpper.length);
+// var randLower = Math.floor(Math.random() * passCriteria.charLower.length);
+// var randSpecial = Math.floor(Math.random() * passCriteria.charSpecial.length);
 
-//2. confirm statements for uppercase, lowercase, numeric, special character - anything that is selected true should be included. These should all be in variables. 
+// console.log(passCriteria.charNumber[randNumb]);
+// console.log(passCriteria.charUpper[randUpper]);
+// console.log(passCriteria.charLower[randLower]);
+// console.log(passCriteria.charSpecial[randSpecial]);
 
-// Variables for password criteria 
 
 
+//3. after all prompts are answered the password will generate in the box. 
 
-//These are variables for the prompts
 
-var askCharLength = "";
+//1st- declare your 4 variables
+var charNumber = ["0","1","2","3","4","5","6","7","8","9","10"];
+var charUpper = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+var charLower = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+var charSpecial = ["~","!","@","#","$","%","^","&","*","(",")","-","_","+","="];
+
+
+var askCharLength;
 var askNumber;
 var askUpper;
 var askLower;
 var askSpecial;
 
-// Character length confirmation
-
-function writePassword() {
-
-  var askCharLength = (prompt("please choose a character length between 8 and 128 characters."))
-
-  //I want to make a function that does not take a response outside of 8-128 characters.
-
-  if (askCharLength < 8 || askCharLength > 128) {
-
-    do { alert("Invalid answer. Please choose between 8 and 128 characters.");
-    var askCharLength = (prompt("please choose a character length between 8 and 128 characters."));}
-
-   while ( askCharLength < 8 || askCharLength > 128 )
 
 
-  }
+
+// 3rd- create your function to generate the password options.
+// in this function should be your prompt asking how many characters with parameters for the length, and all your confirm statements.
+
+function PassOpt() {
+
+
+    var askCharLength = (prompt("please choose a character length between 8 and 128 characters."))
+  
+    //I want to make a function that does not take a response outside of 8-128 characters.
+  
+    if (askCharLength < 8 || askCharLength > 128) {
+  
+      do { alert("Invalid answer. Please choose between 8 and 128 characters.");
+      var askCharLength = (prompt("please choose a character length between 8 and 128 characters."));}
+  
+     while ( askCharLength < 8 || askCharLength > 128 )
+  
+    }
+
+
 
   var askNumber = confirm("would you like your password to use numbers?");
 
@@ -79,102 +90,65 @@ function writePassword() {
     while (askNumber === false && askUpper === false && askLower === false && askSpecial === false)
   }
 
+  var pwOptions = {
+    askCharLength : askCharLength,
+    askNumber : askNumber,
+    askUpper : askUpper,
+    askLower : askLower,
+    askSpecial : askSpecial,
+  }
 
-//3. after all prompts are answered the password will generate in the box.
 
-var passCriteria = {
-  charLength: " " ,
-  charNumber: ["0","1","2","3","4","5","6","7","8","9","10"],
-  charUpper: ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"],
-  charLower: ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"],
-  charSpecial: ["~","!","@","#","$","%","^","&","*","(",")","-","_","+","="],
+  return pwOptions;
+}
+  function getRandom(arr) {
+    var randomIndex = Math.floor(Math.random() * arr.length);
+    var result = arr[randomIndex];
+    return result;
+  }
 
+  function generatePassword() {
+    var options = PassOpt();
+    var result = [];
+    var possibleChar = [];
+    var finalChar = [];
+  
+    if (options.askNumber) {
+      possibleChar = possibleChar.concat(charNumber);
+      finalChar.push(getRandom(charNumber));
+    }
+    if (options.askUpper) {
+      possibleChar = possibleChar.concat(charUpper);
+      finalChar.push(getRandom(charUpper));
+    }
+    if (options.askLower) {
+      possibleChar = possibleChar.concat(charLower);
+      finalChar.push(getRandom(charLower));
+    }
+    if (options.askSpecial) {
+      possibleChar = possibleChar.concat(charNumber);
+      finalChar.push(getRandom(charSpecial));
+    }
+  
+  for (var i=0; i < options.askCharLength; i++) {
+      result.push(getRandom(possibleChar));
+  }
+  
+  for (var i=0; i< finalChar.length; i++) {
+      result[i] = finalChar[i]
+  }
+  return result.join("");
+  }
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
 }
 
-let passwordValue = "";
-let password = "";
-
-//This loop will generate password
-
-for (i = 0; i > askCharLength; i++) {
-
-  if (askNumber === true) {
-    passwordValue = passwordValue + passCriteria.charNumber;
-  }
-  
-  if (askUpper === true) {
-    passwordValue = passwordValue + passCriteria.charUpper;
-  }
-
-  if (askLower === true) {
-    passwordValue = passwordValue + passCriteria.charLower;
-  }
-
-  if (askSpecial === true) {
-    passwordValue = passwordValue + passCriteria.charSpecial;
-  }
-  password = password + passwordValue.charAt(Math.floor(Math.random() * Math.floor(passwordValue.length -1)));
-
-  console.log(password);
-
-  document.getElementById("textarea").value = password;
-  
-}
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
 
 
-
-
-
-
-
-
-
-
-
-//var specificPass = []
-
-// if (askNumber === true) {
-//   passSpecific = passCriteria.charNumber[randNumb];
-// }
-//  if (askUpper === true) {
-//   passSpecific = passCriteria.charUpper[randUpper];
-//  }
-
-// if (askLower === true) {
-//   passSpecific = passCriteria.charLower[randLower];
-// }
-// if (askSpecial === true) {
-//   passSpecific = passCriteria.charSpecial[randSpecial];
-// }
-
-//console.log(passSpecific)
-
-
-
-
-
-
-
-  
-
-
-
-
-  
-}
-
-
-//ALL GENERATE RANDOM FROM MY ARRAYS
-// var randNumb = Math.floor(Math.random() * passCriteria.charNumber.length);
-// var randUpper = Math.floor(Math.random() * passCriteria.charUpper.length);
-// var randLower = Math.floor(Math.random() * passCriteria.charLower.length);
-// var randSpecial = Math.floor(Math.random() * passCriteria.charSpecial.length);
-
-// console.log(passCriteria.charNumber[randNumb]);
-// console.log(passCriteria.charUpper[randUpper]);
-// console.log(passCriteria.charLower[randLower]);
-// console.log(passCriteria.charSpecial[randSpecial]);
-
-
-
-//3. after all prompts are answered the password will generate in the box. 
